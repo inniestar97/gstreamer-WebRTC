2e3c8a36ea3fe74a676bf8dfe50ddffc38d8f4de
Sangin Lee <tkddls0614@gmail.com>
2023-06-29 00:46:03 +0900

    WebRTC web server dev

--- examples/vidrecv-web-server/src/App.tsx
+++ examples/vidrecv-web-server/src/App.tsx
@@ -23,36 +23,70 @@ function App() {
       wsRef.current = new WebSocket(SIGNALING_SERVER_URL);
       console.log(wsRef);
 
-      wsRef.current.onopen = () => {
-        console.log("connected to " + SIGNALING_SERVER_URL);
+      wsRef.current.onopen = (ev: Event) => {
+        console.log("connected to " + SIGNALING_SERVER_URL + ", signaling ready.");
+      };
+
+      wsRef.current.onerror = (ev: Event) => {
+        console.log("WebSocket error");
+      };
+
+      wsRef.current.onclose = (ev: CloseEvent) => {
+        console.log("Websocket Closed");
       };
       
       wsRef.current.onmessage = (msgEvent: MessageEvent) => {
+        if (typeof msgEvent.data === "string") {
+          console.log("Websocket onMessage data type is not currect.");
+          return;
+        } 
+
+        const message = JSON.parse(msgEvent.data);
+
+        let id = "id" in message ? message.id : () => {
+          console.log("There's no 'id' in JSON");
+          return;
+        };
+        let type = "type" in message ? message.type : () => {
+          console.log("There's no 'type' in JSON");
+          return;
+        }
+
+        // if (type == "answer") {
+        //   const sdp = message.description as string;
+        //   pcRef.current?.setRemoteDescription())
+        // }
+
+
       }
     }
 
   }, []);
 
-  const createOffer = async () => {
-    if (!(pcRef.current && wsRef.current)) return;
-    try {
-      const sdp = await pcRef.current.createOffer({
-        offerToReceiveVideo: true,
-        offerToReceiveAudio: true,
-      });
-      await pcRef.current.setLocalDescription(new RTCSessionDescription(sdp));
-      wsRef.current.send(JSON.stringify({
-        "type": "offer",
-        "sdp": sdp
-      }));
-    }
-  };
+  // const createOffer = async () => {
+  //   if (!(pcRef.current && wsRef.current)) return;
+  //   try {
+  //     const sdp = await pcRef.current.createOffer({
+  //       offerToReceiveVideo: true,
+  //       offerToReceiveAudio: true,
+  //     });
+  //     await pcRef.current.setLocalDescription(new RTCSessionDescription(sdp));
+  //     wsRef.current.send(JSON.stringify({
+  //       "type": "offer",
+  //       "sdp": sdp
+  //     }));
+  //   }
+  // };
 
   const buttonOnClick = () => {
     if (!wsRef.current || remoteId === "")  return;
 
     console.log("Offering to " + remoteId + "...");
     pcRef.current = createPeerConnection(pc_config, wsRef.current, remoteId);
+    if (!pcRef.current) {
+      console.log("Peerconnection failed");
+      return;
+    }
 
     /*
      * In this case, button 'clicker' is Offerer.
@@ -61,10 +95,25 @@ function App() {
     const testLabel = "This is the react side label for test";
     console.log("Create Datachannel and VideoTrack with label '" + testLabel + "'");
 
+    const dc = pcRef.current.createDataChannel(testLabel);
 
+    dc.onopen = () => {
+      console.log("DataChannel from " + remoteId + " open.");
+      dc.send("Hello from web");
+    }
 
+    dc.onclose = () => {
+      console.log("DataChannel from " + remoteId + " close.");
+    }
 
-
+    dc.onmessage = (data: any) => {
+      if (typeof data === "string") {
+        console.log("Message from " + remoteId + " : " + data);
+      } else { // show in binary
+        let binaryData: Uint8Array = new Uint8Array(data);
+        console.log("Binary Message from " + remoteId + " : " + binaryData);
+      }
+    }
 
   };
 
